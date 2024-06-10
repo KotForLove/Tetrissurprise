9445bc2271aaba190b09f4bce9d770808b72242d
KotForLove <m.kotkovskyi@ukma.edu.ua>
2024-06-10 11:59:58 +0300

    addding transition picture

--- src/PPO/TransitDialog.java
+++ src/PPO/TransitDialog.java
@@ -1,23 +1,39 @@
 package PPO;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class TransitDialog extends JDialog {
     public TransitDialog(JFrame owner) {
         super(owner, "ALERT!!!", true);
 
-        JButton okButton = new JButton("Become a PPO soldier");
+        // Створення кнопки з текстом "Я Згоден"
+        JButton okButton = new JButton("Я Згоден");
 
-        okButton.addActionListener(e -> dispose());
+        // Додавання слухача подій для кнопки
+        okButton.addActionListener(e -> {
+            // Код для переходу в гру
+            dispose(); // Закриває діалогове вікно
+        });
 
-        add(okButton);
+        // Завантаження та масштабування зображення
+        ImageIcon originalIcon = new ImageIcon("/Users/mark.kotkovskyi/IdeaProjects/Tetrissurprise/src/images/soldier.png"); // Вкажіть шлях до вашого зображення
+        Image originalImage = originalIcon.getImage();
+        Image scaledImage = originalImage.getScaledInstance(400, 300, Image.SCALE_SMOOTH);
+        ImageIcon scaledIcon = new ImageIcon(scaledImage);
+        JLabel imageLabel = new JLabel(scaledIcon);
 
+        // Додавання зображення та кнопки до діалогового вікна
+        setLayout(new BorderLayout());
+        add(imageLabel, BorderLayout.CENTER);
+        add(okButton, BorderLayout.SOUTH);
+
+        // Програвання звуку
         new SoundPlayer("src/sounds/soldier_shout.wav").play();
 
         setSize(400, 400);
-
-        setVisible(true);
-        pack();
         setLocationRelativeTo(owner);
+        setVisible(true);
     }
 }
